18f2cae026facfa64b480dbb5b84a1e10421b8b7
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-08 09:45:03 -0400

    structure/styling startup

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
 
 
 
+var button = $('.btn');
+
+
+
+button.click(function () {
+
+
+console.log(inputValue)
 
 
 
@@ -12,18 +20,12 @@ $(document).ready(function() {
 
 
 
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + $('#city').val() + '&appid=a32ee3008dc5ad65e11f2b980a7a6054')
+    .then(response => response.json())
+    .then(data => console.log(data));
 
-
-
-
-
-
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=Boston&appid=a32ee3008dc5ad65e11f2b980a7a6054')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-
-
-// fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={a32ee3008dc5ad65e11f2b980a7a6054}')
+})
+// fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=a32ee3008dc5ad65e11f2b980a7a6054')
 //     .then((response) => response.json())
 //     console.log(response)
     
