c4c6242c1c4762aadfc3ee9c6f932bd3626fbc20
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-08 17:30:34 -0400

    set up fetches and appended first card

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,32 +2,52 @@ $(document).ready(function() {
 
 
 
-var button = $('.btn');
-
-
-
-button.click(function () {
-
-
-console.log(inputValue)
-
-
-
-
-
-
-
-
-
-
-fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + $('#city').val() + '&appid=a32ee3008dc5ad65e11f2b980a7a6054')
-    .then(response => response.json())
-    .then(data => console.log(data));
-
+var searchBtn = $('#searchBtn');
+searchBtn.click(function(event) {
+event.preventDefault();
+    if ($('#cityInput').val()) {
+        $('#appendCity').append('<button class="citybtn">' + $('#cityInput').val() + '</button>').val($('#cityInput').val());
+        fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + $('#cityInput').val() + '&appid=a32ee3008dc5ad65e11f2b980a7a6054')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+              var lat = data[0].lat;
+              var lon = data[0].lon;
+              $('#lat').text(lat);
+              $('#lon').text(lon);
+        fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=a32ee3008dc5ad65e11f2b980a7a6054')
+            .then(response => response.json())
+            .then(data => {
+              var arr = [];
+                arr.push(data)
+                console.log(arr)
+              var cityName = arr[0].name;
+              var temp = arr[0].main.temp;
+              var wind = arr[0].wind.speed;
+              var humidity = arr[0].main.humidity;
+              var icon = arr[0].weather[0].icon;
+              $('#tempSpan').removeClass('invisible');
+              $('#windSpan').removeClass('invisible');
+              $('#humiditySpan').removeClass('invisible');
+              $('#cityHeader').text(cityName);
+              $('#temp').text(temp);
+              $('#wind').text(wind);
+              $('#humidity').text(humidity);
+              $('#icon').addClass('src').attr("http://openweathermap.org/img/wn/" + icon + ".png");
+            });
+        });
+        $('#cityInput').val('');
+    }
+    $('#cityHeader').text($(this)[0].name);
 })
-// fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=a32ee3008dc5ad65e11f2b980a7a6054')
-//     .then((response) => response.json())
-//     console.log(response)
+
+
+$(document).on('click', '.citybtn', function() {
+var btnText = $(this).text();
+console.log(btnText);
+})
+
+
     
 
 
